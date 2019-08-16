a2d6a751110b298ef5efd5f00b7e51850476ff56
Thyago Dias <thyago.dias@unemat.br>
2019-08-16 10:02:07 -0400

    Melhora identação do código

--- client/js/app/models/ListaNegociacoes.js
+++ client/js/app/models/ListaNegociacoes.js
@@ -1,17 +1,17 @@
 class ListaNegociacoes {
-    
-    constructor() {
-        
-        this._negociacoes = [];
-    }
-    
-    adiciona(negociacao) {
-        
-        this._negociacoes.push(negociacao);
-    }
-    
-    get negociacoes() {
-        
-        return [].concat(this._negociacoes);
-    }
+
+  constructor() {
+
+    this._negociacoes = [];
+  }
+
+  adiciona(negociacao) {
+
+    this._negociacoes.push(negociacao);
+  }
+
+  get negociacoes() {
+
+    return [].concat(this._negociacoes);
+  }
 }
