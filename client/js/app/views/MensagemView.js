f2ad02e35b49fd7e52c8935b8fe37ccb828feabc
Thyago Dias <thyago.dias@unemat.br>
2019-08-22 10:37:48 -0400

    Aula 6 atividade 03

--- client/js/app/views/MensagemView.js
+++ client/js/app/views/MensagemView.js
@@ -1,15 +1,10 @@
-class MensagemView {
+class MensagemView extends View {
+
   constructor(elemento) {
-    this._elemento = elemento;
+    super(elemento)
   }
 
-  template(model) {
-    return model.texto ? `
-      <p class="alert alert-info">${model.texto}</p>
-    ` : '<p></p>'
-  }
-
-  update(model) {
-    this._elemento.innerHTML = this.template(model)
+  _template(model) {
+    return model.texto ? `<p class="alert alert-info">${model.texto}</p>` : '<p></p>';
   }
 }
--- client/js/app/views/NegociacoesView.js
+++ client/js/app/views/NegociacoesView.js
@@ -1,7 +1,7 @@
-class NegociacoesView {
+class NegociacoesView extends View {
 
   constructor(elemento) {
-    this._elemento = elemento;
+    super(elemento)
   }
 
   _template(model) {
@@ -37,11 +37,6 @@ class NegociacoesView {
         </tfoot>
       </table>
     `
-  }
-
-  update(model) {
-    this._elemento.innerHTML = this._template(model);
-
   }
 }
 
